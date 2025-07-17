e5780f6a87f9e7150fccfa3665ff11d290198d49
Parth Vaghasiya <143266510+Parth515@users.noreply.github.com>
2025-07-17 11:27:34 +0200

    Create Day_9.py

---
+++ Beginner/Day_9.py
@@ -0,0 +1,36 @@
+# Blind Auction Project
+
+# Functionality
+
+# Each person writes their name and bid.
+# The program asks if there are others who need to bid. If so, then the computer clears the output (prints several blank lines) then loops back to asking name and bid.
+# Each person's name and bid are saved to a dictionary.
+# Once all participants have placed their bid, the program works out who has the highest bid and prints it.
+
+from art import logo
+print(logo)
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
