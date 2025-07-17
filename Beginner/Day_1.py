9f6f882e892f5fe1beffd6db93f9931cdc791081
Parth Vaghasiya <143266510+Parth515@users.noreply.github.com>
2025-07-17 11:10:02 +0200

    Update Day_1.py

--- Beginner/Day_1.py
+++ Beginner/Day_1.py
@@ -1,6 +1,6 @@
 # Band Name Generator Project
 
-# Create a greeting for your program.
+# Create a greeting for the program.
 # Ask the user for the city that they grew up in and store it in a variable.
 # Ask the user for the name of a pet and store it in a variable.
 # Combine the name of their city and pet and show them their band name.
