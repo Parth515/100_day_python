49ad498e697c0ca619c107f96eb1de45299b88a3
Parth Vaghasiya <143266510+Parth515@users.noreply.github.com>
2025-08-03 09:50:09 +0200

    Create snake.py

---
+++ Intermediate/Day_20/snake.py
@@ -0,0 +1,51 @@
+from turtle import Turtle
+
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.start_pos = 0
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(3):
+            tim = Turtle(shape="square")
+            tim.color("white")
+            tim.penup()
+            tim.goto(x=self.start_pos, y=0)
+            self.start_pos -= 20
+            self.segments.append(tim)
+
+    def move_forward(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+        '''if self.segments[0].heading() == 90:
+            self.segments[0].left(90)
+        elif self.segments[0].heading() == 270:
+            self.segments[0].right(90)'''
