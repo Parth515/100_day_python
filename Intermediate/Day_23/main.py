b7d4ea3cd7ad661213126f3e4edf8e4fdef96cb8
Parth Vaghasiya <143266510+Parth515@users.noreply.github.com>
2025-08-07 17:35:33 +0200

    Create main.py

---
+++ Intermediate/Day_23/main.py
@@ -0,0 +1,42 @@
+# The Turtle Crossing the Road Game
+
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("white")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.p_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    cars.car()
+    cars.move()
+
+    # car collision with turtle
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # level success
+    if player.is_at_finish_line():
+        player.go_to_start()
+        cars.level_up()
+        scoreboard.increase_level()
+
+
+
+screen.exitonclick()
