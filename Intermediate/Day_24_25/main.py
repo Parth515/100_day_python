e081bff9d33ef2c53f4f7126b38ee2ce40321c0b
Parth Vaghasiya <143266510+Parth515@users.noreply.github.com>
2025-08-09 10:55:59 +0200

    Update main.py

--- Intermediate/Day_24_25/main.py
+++ Intermediate/Day_24_25/main.py
@@ -1,1 +1,53 @@
+# The State Guessing Game
+
+import turtle
+import pandas
+
+FONT = ("Courier", 6, "normal")
+
+guessed_states = []
+
+
+'''def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+'''
+
+state_data = pandas.read_csv("50_states.csv")
+state_names = state_data.state.to_list()
+
+def write_state(st, x_1, y_1):
+    tim = turtle.Turtle()
+    tim.hideturtle()
+    tim.penup()
+    tim.goto(x_1, y_1)
+    tim.write(f"{st}", align="left", font=FONT)
+
+while len(guessed_states) < 50:
+    screen = turtle.Screen()
+    screen.title("U.S. State Game")
+    image = "blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states are correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_state = []
+        for state in state_names:
+            if state not in guessed_states:
+                missing_state.append(state)
+        df = pandas.DataFrame(missing_state)
+        df.to_csv("state_to_learn.csv")
+        break
+
+    if answer_state in state_names:
+        guessed_states.append(answer_state)
+        state = state_data[state_data.state == answer_state]
+        x_cord = state.x.item()
+        y_cord = state.y.item()
+        write_state(state.state.item(),x_cord,y_cord)
+
+
 
